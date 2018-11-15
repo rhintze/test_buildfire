f41ad4645eaf0d07b7290be1def0a29c99049c2f
Ricardo Ramirez <ramirezhintze@gmail.com>
2018-11-14 18:43:59 -0600

    Added Header element,added font-style and resetcss,  applied styles to main heading and button,

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+function loadTasks(taskList){
+    let container = document.getElementById('taskContainer');
+    container.innerHTML='';
+
+    taskList.forEach((task,i)=>{
+        let divTask = document.createElement('div');
+        divTask.className="taskCard";
+        divTask.innerHTML = `
+            ${task.img?"<img src='" + task.img + "' />":""}
+            <h4> <input type="checkbox" ${task.completed?"checked":""} title="check completed">  ${task.title + ' ' + i}</h4>
+            <span>created on ${task.createdOn} by ${task.createdBy}</span>
+            <p>${task.description}</p>
+            <span>Due on ${task.dueDate}</span>            
+        `;
+
+        if(task.completed)
+            divTask.classList.add("taskCompleted");
+        else if(task.dueDate < Date.now() )
+            divTask.classList.add("taskLate");
+
+
+        container.appendChild(divTask);
+    });
+}
+
+
+loadTasks(taskList);
+
+
+function addTask(task){
+    taskList.unshift(task);
+    loadTasks(taskList);
+}
---
+++ js/taskList.js
@@ -0,0 +1,56 @@
+
+
+let taskList= [
+    {
+        title:"Task Title"
+        ,img:"https://picsum.photos/1600/900"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back." /// <= Pretty long
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2020") /// Has enough time to finish
+    }
+    ,{
+        title:"Task Title"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2018") /// <== Past due.
+    }
+    ,{
+        title:"Task Title"
+        ,img:"https://picsum.photos/1600/900?v1" /// <=== big image
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2020")
+        ,completed:true
+    }
+    ,{
+        title:"Task Title"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2020")
+    }
+    ,{
+        title:"Task Title"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2018")
+    }
+    ,{
+        title:"Task Title"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2018")
+    }
+    ,{
+        title:"Task Title"
+        ,description:"This is a description of the task. Its important to make sure the task manager is able to deal with really long descriptions. Even if its a list of W's back to back."
+        ,createdOn:new Date()
+        ,createdBy:"Jane Smith"
+        ,dueDate:new Date("01/01/2020")
+    }
+];
